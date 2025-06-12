883d1499928529046b6738530f907247eae28559
KTrordu <alptug.yilmaz@outlook.com>
2025-06-12 11:44:38 +0300

    added seq_file.c

---
+++ Final/seq_file.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *cfPtr; // clients.txt pointer
+    
+    // fopen opens the file. Exit program if unable to create file
+    if ((cfPtr = fopen("clients.txt", "w")) == NULL)
+    {
+        puts("File could not be opened.");
+    }
+    else
+    {
+        puts("Enter the account, name and balance. ");
+        puts("Enter EOF to end input. ");
+        printf("%s", "? ");
+
+        unsigned int account; // account number
+        char name[30]; // account name
+        double balance; // account balance
+
+        scanf("%d%29s%lf", &account, name, &balance);
+
+        // write account, name and balance into file with fprintf
+        while (!feof(stdin))
+        {
+            fprintf(cfPtr, "%d %s %.2f\n", account, name, balance);
+            printf("%s", "? ");
+            scanf("%d%29s%lf", &account, name, &balance);
+        }
+
+        fclose(cfPtr); // fclose closes the file
+    }
+
+    return 0;
+}
