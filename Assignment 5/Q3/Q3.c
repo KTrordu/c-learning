8bc67eeb7efe9a3a00a2d3aee7aae953a1accb9b
KTrordu <alptug.yilmaz@outlook.com>
2025-06-13 20:49:42 +0300

    added Q3

---
+++ Assignment 5/Q3/Q3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *lwPtr, *lrPtr;
+    if ((lwPtr = fopen("errors.log", "w")) == NULL)
+    {
+        puts("errors.log could not be opened.\n");
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        if ((lrPtr = fopen("logs.log", "r")) == NULL)
+        {
+            puts("logs.log could not be opened.\n");
+            return EXIT_FAILURE;
+        }
+        else
+        {
+            char line[1000];
+            int line_number = 0;
+
+            while(fgets(line, sizeof(line), lrPtr))
+            {
+                line_number++;
+
+                line[strcspn(line, "\n")] = 0;
+
+                if (strstr(line, "[ERROR]"))
+                {
+                    fprintf(lwPtr, "%s\n", line);
+                }
+            }
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
