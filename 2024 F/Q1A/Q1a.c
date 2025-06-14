30d04fa6a1b0db82d405484a4c9c539ac05fdc55
KTrordu <alptug.yilmaz@outlook.com>
2025-06-14 03:05:32 +0300

    added Q1a

---
+++ 2024 F/Q1A/Q1a.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *fPtr;
+    if ((fPtr = fopen(argv[1], "r")) == NULL)
+    {
+        puts("The file could not be opened.\n");
+    }
+    else
+    {
+        int space_count = 0;
+
+        while (!feof(fPtr))
+        {
+            char letter = fgetc(fPtr);
+            if (letter == ' ')
+            {
+                space_count++;
+            }
+        }
+        printf("Number of spaces between the words: %d", space_count);
+    }
+    fclose(fPtr);
+
+    return EXIT_SUCCESS;
+}
